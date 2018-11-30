e80f07cb078042fdaf7b9898284eb77bf438ce84
andrei nfs <andreinfs@yahoo.com>
2018-11-30 18:29:10 +0200

    Contine : -Hotkey pe enter                  -Focus automat pe textbar

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 92);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Verificare";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(156, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(340, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(225, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(195, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Verifica daca numarul este prim";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(643, 390);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Numere prime ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -15,6 +15,66 @@ namespace WindowsFormsApp2
         public Form1()
         {
             InitializeComponent();
+            this.ActiveControl = textBox1;
+            textBox1.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   
+
+          
+            int num1 , k=0;
+                try
+                {
+                    num1 = Convert.ToInt32(textBox1.Text);
+                }
+                catch (Exception)
+                {
+                    
+                    num1 = 0;
+                }
+            if (num1 <= 0)
+            {
+                this.BackColor = Color.White;
+                label1.Text = ("Introduce un numar valid");
+            }
+            else
+            {
+                if (num1 != 1)
+                {
+                    for (int a = 2; a <= num1 / 2; a++)
+                    {
+                        if (num1 % a == 0)
+                            k++;
+                    }
+                    if (k != 0)
+                    {
+                        this.BackColor = Color.DarkRed;
+                        label1.Text = ("   Numarul nu este prim");
+                    }
+                    else
+                    {
+                        this.BackColor = Color.DarkGreen;
+                        label1.Text = ("   Numarul este prim");
+                    }
+                }
+                else
+                {
+                    if (num1 == 1)
+                    {
+                        this.BackColor = Color.DarkRed;
+                        label1.Text = ("   Numarul nu este prim");
+                    }
+                }
+            }
+
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
         }
     }
 }
